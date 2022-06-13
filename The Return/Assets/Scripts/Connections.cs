2d1eb88aa833f81b0ecbf448b127c95245cba1de
Haardik <ravat.1@iitj.ac.in>
2022-06-14 02:09:42 +0530

    World Data Structure

---
+++ The Return/Assets/Scripts/Connections.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Connections
+{
+
+    public string ConnectionName;
+
+    public string Description;
+
+    public Location location;
+
+    public bool connectionEnabled;
+   
+}
---
+++ The Return/Assets/Scripts/Location.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Location : MonoBehaviour
+{
+    public string Location_Name;
+    [TextArea]
+    public string Description;
+
+    public Connections[] connections;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
