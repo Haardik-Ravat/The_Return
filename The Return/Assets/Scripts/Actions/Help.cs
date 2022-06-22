53ed5a356e77b88c312423ed53be37ab42fa29e9
Haardik <ravat.1@iitj.ac.in>
2022-06-22 15:38:20 +0530

    Action Help

---
+++ The Return/Assets/Scripts/Actions/Help.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Actions/Help")]
+public class Help : Action
+{
+    public override void Respond(GameController controller, string verb)
+    {
+        controller.curText.text = "Type a verb followed by a noun(e.g. \"go north\")";
+        controller.curText.text = "\nAllowed Verbs:\n Go, Examine, Get, Use, Inventory, TalkTo, Say, Help";
+
+    }
+}
