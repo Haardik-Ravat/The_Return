f518fe4ec5a9fee58a6430d73c0a01083f700034
Haardik <ravat.1@iitj.ac.in>
2022-06-25 15:01:10 +0530

    editing C# scripts

--- The Return/Assets/Scripts/Actions/Get.cs
+++ The Return/Assets/Scripts/Actions/Get.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Get : MonoBehaviour
+
+[CreateAssetMenu(menuName = "Actions/Get")]
+public class Get : Action
 {
+    public override void Respond(GameController controller, string verb)
+    {
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
+
